a20800ea451a148fd90ca9e41771dfca5aa00a41
Dheeru01 <141866112+Dheeru01@users.noreply.github.com>
2025-01-11 19:08:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose"
 export const connectDB = async()=>{
     const {connection} = await mongoose.connect(process.env.MONGO_URI,{
         serverSelectionTimeoutMS: 30000, // 30 seconds
-    socketTimeoutMS: 0, // No socket timeout
-    connectTimeoutMS: 0 
+        socketTimeoutMS: 0, // No socket timeout
+        connectTimeoutMS: 0,
+        tls: true, // Enable TLS/SSL
+        tlsAllowInvalidCertificates: false // Ensure valid certificates
     })
     console.log(`Mongo DB connected: ${connection.host}`)
 }
